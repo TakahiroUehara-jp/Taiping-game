e8349a93cdc3128828e1f220827a10cfcb3dac4c
ryouma1016 <vuht6633@icloud.com>
2025-01-30 12:35:17 +0900

    Add files via upload

---
+++ 英単語リスト.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 30 12:24:02 2025
+
+@author: futof
+"""
+
+#英単語のリスト
+dic_word = [
+    "apple",
+    "book",
+    "cat",
+    "dog",
+    "egg",
+    "fish",
+    "grape",
+    "house",
+    "ice",
+    "juice",
+    "chocolate",
+    "lemon",
+    "moon",
+    "university",
+    "orange",
+    "pencil",
+    "queen",
+    "rabbit",
+    "star",
+    "tree"
+]
